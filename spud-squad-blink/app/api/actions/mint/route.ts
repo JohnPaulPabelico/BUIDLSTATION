379910b9011599719ad0caff094893bca9c733b3
Jp Pabelico <89491569+JohnPaulPabelico@users.noreply.github.com>
2024-07-25 03:55:07 +0800

    Add umi transaction

--- spud-squad-blink/app/api/actions/mint/route.ts
+++ spud-squad-blink/app/api/actions/mint/route.ts
@@ -6,6 +6,31 @@ import {
   createPostResponse,
 } from "@solana/actions";
 import { transferSolTransaction } from "./transaction";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import {
+  generateSigner,
+  transactionBuilder,
+  some,
+  publicKey,
+} from "@metaplex-foundation/umi";
+import {
+  fetchCandyMachine,
+  mintV2,
+  mplCandyMachine,
+  safeFetchCandyGuard,
+} from "@metaplex-foundation/mpl-candy-machine";
+import { mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
+import { setComputeUnitLimit } from "@metaplex-foundation/mpl-toolbox";
+import { clusterApiUrl } from "@solana/web3.js";
+import { toWeb3JsLegacyTransaction } from "@metaplex-foundation/umi-web3js-adapters";
+import { walletAdapterIdentity } from "@metaplex-foundation/umi-signer-wallet-adapters";
+// import { useWallet } from "@solana/wallet-adapter-react";
+
+const ENDPOINT = process.env.NEXT_PUBLIC_RPC || clusterApiUrl("devnet");
+const candyMachineAddress = publicKey(
+  "3vmJZBmhNUvrnrGmnzjvmUWuQnck2XhZEFoAWBd8q9d3"
+);
+const treasury = publicKey("9QZqqJfKRuoGKTaCgUvjQMMUNpaxhPC3fvn2y8iPZ4uU");
 
 export const GET = async (req: Request) => {
   const payload: ActionGetResponse = {
@@ -24,10 +49,48 @@ export const OPTIONS = GET;
 
 export const POST = async (req: Request) => {
   const body: ActionPostRequest = await req.json();
-  const transaction = await transferSolTransaction({
-    from: body.account,
-    amount: 1,
-  });
+
+  // const wallet = useWallet();
+
+  const umiTransaction = async () => {
+    const umi = createUmi(ENDPOINT)
+      // .use(walletAdapterIdentity(wallet))
+      .use(mplCandyMachine())
+      .use(mplTokenMetadata());
+
+    const candyMachine = await fetchCandyMachine(umi, candyMachineAddress);
+    const candyGuard = await safeFetchCandyGuard(
+      umi,
+      candyMachine.mintAuthority
+    );
+
+    try {
+      const nftMint = generateSigner(umi);
+      const transaction = await transactionBuilder()
+        .add(setComputeUnitLimit(umi, { units: 800_000 }))
+        .add(
+          mintV2(umi, {
+            candyMachine: candyMachine.publicKey,
+            candyGuard: candyGuard?.publicKey,
+            nftMint,
+            collectionMint: candyMachine.collectionMint,
+            collectionUpdateAuthority: candyMachine.authority,
+            mintArgs: {
+              solPayment: some({ destination: treasury }),
+            },
+          })
+        )
+        .build(umi);
+
+      return transaction;
+    } catch (error) {
+      console.error("Error creating transaction:", error);
+      throw error;
+    }
+  };
+
+  const myUmiTransaction = await umiTransaction();
+  const transaction = toWeb3JsLegacyTransaction(myUmiTransaction);
 
   const payload: ActionPostResponse = await createPostResponse({
     fields: {
