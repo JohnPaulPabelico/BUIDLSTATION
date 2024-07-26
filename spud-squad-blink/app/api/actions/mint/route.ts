4c1a9a7ae5469e38af95420618c3c5c8dcc85d33
Jp Pabelico <89491569+JohnPaulPabelico@users.noreply.github.com>
2024-07-26 09:49:08 +0800

    Abstracted mintTransaction

--- spud-squad-blink/app/api/actions/mint/route.ts
+++ spud-squad-blink/app/api/actions/mint/route.ts
@@ -5,31 +5,7 @@ import {
   ActionPostResponse,
   createPostResponse,
 } from "@solana/actions";
-import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
-import {
-  generateSigner,
-  transactionBuilder,
-  some,
-  publicKey,
-  createNoopSigner,
-  signerIdentity,
-} from "@metaplex-foundation/umi";
-import {
-  fetchCandyMachine,
-  mintV2,
-  mplCandyMachine,
-  safeFetchCandyGuard,
-} from "@metaplex-foundation/mpl-candy-machine";
-import { mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
-import { setComputeUnitLimit } from "@metaplex-foundation/mpl-toolbox";
-import { clusterApiUrl, Connection, Keypair, PublicKey } from "@solana/web3.js";
-import { toWeb3JsLegacyTransaction } from "@metaplex-foundation/umi-web3js-adapters";
-
-const ENDPOINT = process.env.NEXT_PUBLIC_RPC || clusterApiUrl("devnet");
-const candyMachineAddress = publicKey(
-  "3vmJZBmhNUvrnrGmnzjvmUWuQnck2XhZEFoAWBd8q9d3"
-);
-const treasury = publicKey("9QZqqJfKRuoGKTaCgUvjQMMUNpaxhPC3fvn2y8iPZ4uU");
+import { mintTransaction } from "./mintTransaction";
 
 export const GET = async (req: Request) => {
   const payload: ActionGetResponse = {
@@ -49,63 +25,16 @@ export const OPTIONS = GET;
 export const POST = async (req: Request) => {
   const body: ActionPostRequest = await req.json();
 
-  const myPublicKey = publicKey(body.account);
-  const mySigner = createNoopSigner(myPublicKey);
+  const userPubKey = body.account;
 
-  const umiTransaction = async () => {
-    const umi = createUmi(ENDPOINT)
-      .use(signerIdentity(mySigner))
-      .use(mplCandyMachine())
-      .use(mplTokenMetadata());
-
-    const candyMachine = await fetchCandyMachine(umi, candyMachineAddress);
-    const candyGuard = await safeFetchCandyGuard(
-      umi,
-      candyMachine.mintAuthority
-    );
-    const connection = new Connection(ENDPOINT);
-    const { blockhash, lastValidBlockHeight } =
-      await connection.getLatestBlockhash();
-    try {
-      const nftMint = generateSigner(umi);
-
-      const tx = await transactionBuilder()
-        .add(setComputeUnitLimit(umi, { units: 800_000 }))
-        .add(
-          mintV2(umi, {
-            candyMachine: candyMachine.publicKey,
-            candyGuard: candyGuard?.publicKey,
-            nftMint,
-            collectionMint: candyMachine.collectionMint,
-            collectionUpdateAuthority: candyMachine.authority,
-            mintArgs: {
-              solPayment: some({ destination: treasury }),
-            },
-          })
-        )
-        .setBlockhash(blockhash)
-        .build(umi);
-
-      const web3JsTransaction = toWeb3JsLegacyTransaction(tx);
-
-      web3JsTransaction.partialSign({
-        publicKey: new PublicKey(nftMint.publicKey.toString()),
-        secretKey: nftMint.secretKey,
-      });
-
-      return web3JsTransaction;
-    } catch (error) {
-      console.error("Error creating transaction:", error);
-      throw error;
-    }
-  };
-
-  const transaction = await umiTransaction();
+  const transaction = await mintTransaction({
+    toAddress: userPubKey,
+  });
 
   const payload: ActionPostResponse = await createPostResponse({
     fields: {
       transaction,
-      message: `Send 1 SOL`,
+      message: `Hello there ${body.account}`,
     },
   });
   return Response.json(payload, {
